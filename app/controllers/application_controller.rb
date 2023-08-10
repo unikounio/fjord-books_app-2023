3028cdf90f517af94496f8b7f759739b41d4de85
unikounio <bubbbble428@gmail.com>
2023-08-10 14:17:23 +0900

    ログイン・ログアウト機能が完成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    books_path
+  end
 end
