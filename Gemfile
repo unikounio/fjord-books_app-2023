717471429422511d2df185f722db37621755d932
unikounio <bubbbble428@gmail.com>
2023-08-10 20:40:53 +0900

    devise-i18n gemを導入してDeviseによる表示箇所を日本語化した

--- Gemfile
+++ Gemfile
@@ -80,4 +80,5 @@ end
 
 gem 'carrierwave'
 gem 'devise'
+gem 'devise-i18n'
 gem 'rails-i18n'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,14 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  
+
   private
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_in_path_for(_resource_or_scope)
+    books_path
+  end
+
+  def after_sign_out_path_for(_resource_or_scope)
     books_path
   end
 end
