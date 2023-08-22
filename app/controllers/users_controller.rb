5b3f6c707324f2fdddfb7f694011d03fca94dba2
unikounio <bubbbble428@gmail.com>
2023-08-22 23:49:28 +0900

    ユーザー一覧画面、ユーザー詳細画面、ユーザー編集画面にアイコンを表示するようにした

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,11 @@
 
 class UsersController < ApplicationController
   def index
-    @users = User.order(:id).page(params[:page])
+    @users = User.with_attached_icon.order(:id).page(params[:page])
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.with_attached_icon.find(params[:id])
   end
 
   private
