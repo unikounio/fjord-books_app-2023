26646335e217a2e4c5cb960adedc80334b922afa
unikounio <bubbbble428@gmail.com>
2023-08-27 13:30:55 +0900

    コメントの削除機能を実装した

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,15 +1,30 @@
 class CommentsController < ApplicationController
+  before_action :set_commentable
+
   def create
-    commentable_type = params[:comment][:commentable_type].constantize
-    commentable = commentable_type.find(params[:comment][:commentable_id])
-    comment = current_user.comments.new(comment_params)
+    comment = @commentable.comments.build(comment_params)
+    comment.user = current_user
     comment.save
-    redirect_to polymorphic_path(commentable)
+    redirect_to polymorphic_path(@commentable)
   end
+
+  def destroy
+    comment = Comment.find(params[:id])
+    comment.destroy if comment.user == current_user
+    redirect_to polymorphic_path(@commentable)
+  end 
 
   private
 
+  def set_commentable
+    if params[:book_id]
+      @commentable = Book.find(params[:book_id])
+    else
+      @commentable = Report.find(params[:report_id])
+    end
+  end
+
   def comment_params
-    params.require(:comment).permit(:comment, :commentable_type, :commentable_id)
+    params.require(:comment).permit(:comment)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   resources :reports do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: [:create]
+    resources :comments, only: [:create, :destroy]
   end
   resources :users, only: %i(index show)
 end
