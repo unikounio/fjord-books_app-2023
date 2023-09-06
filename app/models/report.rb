33cccb337833332bb581f6d7260187068d5a12e7
unikounio <bubbbble428@gmail.com>
2023-09-06 21:10:55 +0900

    アソシエーションを設定した

--- app/models/report.rb
+++ app/models/report.rb
@@ -3,6 +3,10 @@
 class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
+  has_many :active_mentions, class_name: "Mention", foreign_key: "mentioning_report_id"
+  has_many :passive_mentions, class_name: "Mention", foreign_key: "mentioned_report_id"
+  has_many :mentioning_reports, through: :active_mentions
+  has_many :mentioned_reports, through: :passive_mentions
 
   validates :title, presence: true
   validates :content, presence: true
