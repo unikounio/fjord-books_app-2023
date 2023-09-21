c9bf6d0f21977f0b315eb7cd5666e32bd59edec8
unikounio <bubbbble428@gmail.com>
2023-09-22 02:30:43 +0900

    コメントのビューファイルに書いていたロジックをモデルに定義

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,12 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :user
   validates :comment, presence: true
+  
+  def display_name_or_email
+    if self.user.name.present?
+      self.user.name
+    else
+      self.user.email
+    end
+  end
 end
