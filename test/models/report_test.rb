46eb8e8567705778d5dca2acc33a8077e980f499
unikounio <bubbbble428@gmail.com>
2023-10-12 21:07:12 +0900

    Reportクラスのインスタンスメソッドのテストを書いた

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,18 @@
 require 'test_helper'
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @report = reports(:daily_alice)
+  end
+
+  test "#editable?" do
+    assert @report.editable?(users(:alice))
+    assert_not @report.editable?(users(:bob))
+  end
+
+  test "#created_on" do
+    fixed_time = Time.zone.local(2023, 10, 12, 20, 0, 0)
+    @report.created_at = fixed_time
+    assert_equal fixed_time.to_date, @report.created_on
+  end
 end
